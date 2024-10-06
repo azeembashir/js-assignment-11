36831708f15fb19c8c7dfae838233fccd82f7779
Azeem Bashir25 <148478162+azeembashir@users.noreply.github.com>
2024-10-06 11:13:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// task 1
+// var number = prompt("enter a positive integer");
+// var value1 = Math.round(number);
+// var value2 = Math.floor(number);
+// var value3 = Math.ceil(number);
+// document.write("Number" + number + "<br>");
+// document.write("roundOff : " + value1 + "<br>");
+// document.write("floor : " + value2 + "<br>");
+// document.write("Ceil : " + value3);
+
+// task 2
+// var number = prompt("enter a positive integer");
+// var value1 = Math.round(number);
+// var value2 = Math.floor(number);
+// var value3 = Math.ceil(number);
+// document.write("Number" + number + "<br>");
+// document.write("roundOff : " + value1 + "<br>");
+// document.write("floor : " + value2 + "<br>");
+// document.write("Ceil : " + value3);
+
+// task 3
